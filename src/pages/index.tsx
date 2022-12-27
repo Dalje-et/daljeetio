11d3111790ab3d3f90189ccbd5206a5f8950ee57
Dalje-et <daljeet-sandu@hotmail.de>
2022-12-27 23:10:43 +0100

    fix overflow of cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 import { graphql } from "gatsby";
 
-
-import About from "@/components/Index/About";
-// import ListNote from "@/components/ListNote";
-import Link from "@/components/GatsbyLink";
 import Layout from "@/components/Layout";
 import Button from "@/components/Button";
+import Card from "@/components/Card";
 
 import {
   HERO_TITLE,
-  HERO_SUBTITLE,
-  SITE_TITLE,
-  SITE_DESCRIPTION,
+  HERO_SUBTITLE
 } from "../../config";
 
 const IndexPage = ({ data, pageContext }) => {
@@ -28,7 +23,22 @@ const IndexPage = ({ data, pageContext }) => {
       <h2 className="text-3xl text-slate-600 mb-6 mt-0">{HERO_SUBTITLE}</h2>
       <Button to="/blog/" label="See Posts →" />
       
-      <About />
+      <div className="mt-16">
+        <div className="grid grid-cols-twoColumn gap-8 py-2 px-2">
+          <Card url="/blog/">
+            <div className="flex flex-col py-3 px-4">
+              <span className="font-mono text-emerald-700">NEW</span>
+              <span className="font-bold text-lg">📖 Blog →</span>
+            </div>
+          </Card>
+          <Card url="/about/">
+            <div className="flex flex-col py-3 px-4">
+              <span className="font-mono text-emerald-700">NEW</span>
+              <span className="font-bold text-lg">👨🏽‍💻 About Me →</span>
+            </div>
+          </Card>
+        </div>
+      </div>
     </Layout>
   );
 };
