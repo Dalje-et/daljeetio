b75235951d3b58796abcf1132ca5335b4efc1010
Dalje-et <daljeet-sandu@hotmail.de>
2022-12-27 02:06:41 +0100

    commit current state; remember that About.tsx has to be renamed

--- src/components/Index/About.tsx
+++ src/components/Index/About.tsx
@@ -6,18 +6,16 @@ export default function About() {
   return (
     <div className="mt-16">
       <div className="grid grid-cols-2 gap-8 h-32 w-full">
+        <Card url="/about/">
+          <div className="flex flex-col py-3 px-4">
+            <span className="font-mono text-emerald-700">NEW</span>
+            <span className="font-bold text-lg">👨🏽‍💻 About Me →</span>
+          </div>
+        </Card>
         <Card url="/blog/">
           <div className="flex flex-col py-3 px-4">
             <span className="font-mono text-emerald-700">NEW</span>
-            <div>02</div>
-            <div>03</div>
-          </div>
-        </Card>
-        <Card>
-          <div className="flex flex-col py-3 px-4">
-            <span className="font-mono text-red-700">OLD</span>
-            <div>02</div>
-            <div>03</div>
+            <span className="font-bold text-lg">📖 Blog →</span>
           </div>
         </Card>
       </div>
