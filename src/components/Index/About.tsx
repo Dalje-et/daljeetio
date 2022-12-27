800b31f41cde037a127db3e834c3d4b16dcbc3ca
Dalje-et <daljeet-sandu@hotmail.de>
2022-12-27 12:38:45 +0100

    change order of about me and blog card

--- src/components/Index/About.tsx
+++ src/components/Index/About.tsx
@@ -7,16 +7,16 @@ export default function About() {
     <div className="mt-16 h-full">
       { /* need to give the grid a padding as it otherwise becomes hidden once we hover on it */}
       <div className="grid grid-cols-twoColumn gap-8 overflow-scroll py-2 px-2">
-        <Card url="/about/">
-          <div className="flex flex-col py-3 px-4">
-            <span className="font-mono text-emerald-700">NEW</span>
-            <span className="font-bold text-lg">👨🏽‍💻 About Me →</span>
-          </div>
-        </Card>
         <Card url="/blog/">
           <div className="flex flex-col py-3 px-4">
             <span className="font-mono text-emerald-700">NEW</span>
             <span className="font-bold text-lg">📖 Blog →</span>
+          </div>
+        </Card>
+        <Card url="/about/">
+          <div className="flex flex-col py-3 px-4">
+            <span className="font-mono text-emerald-700">NEW</span>
+            <span className="font-bold text-lg">👨🏽‍💻 About Me →</span>
           </div>
         </Card>
       </div>
